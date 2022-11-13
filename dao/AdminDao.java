78d1409773bbeb3236a0c16c77308dfe25b85ce2
Saurabh Magdum <101392340+saurabhvmagdum@users.noreply.github.com>
2022-11-13 15:13:00 +0530

    Delete AdminDao.java

--- dao/AdminDao.java
+++
@@ -1,8 +0,0 @@
-package com.masai.dao;
-
-import com.masai.exceptions.adminException;
-
-public interface AdminDao {
-	public boolean LoginAdmin(String username , String password) throws adminException;
-	
-}
